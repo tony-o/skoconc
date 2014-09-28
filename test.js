5c5cd8971fe05a6c2313b1d59bcd809a196e174b
Tony O'Dell <tony.odell@live.com>
2014-09-28 15:32:13 -0700

    updating for npm v2

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 var l = require("./skoconc.js");
 
-l.max(3);
+l.max(1);
 
-for(var i = 0; i < 500; i++){
+for(var i = 0; i < 10; i++){
   l.push(function(a,cb){
     setTimeout(function(){
       cb();
       if(a == 100){ l.max(25); }
       console.log("i:" + a);
       cb(); //doesn't do anything..
-    },i);
+    },1000 - (i*i*2));
   }, i);
 }
 
